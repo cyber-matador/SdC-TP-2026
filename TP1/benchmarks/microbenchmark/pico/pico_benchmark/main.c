b026946739a47cf2872c52320d3e8122f0b8eade
Valentino Renaudo <valentinorenaudo@gmail.com>
2026-03-29 19:38:05 -0300

    feat(pico): replicate Arduino-style int/float microbenchmark, set CPU freq, and log comparable results

--- TP1/benchmarks/microbenchmark/pico/pico_benchmark/main.c
+++ TP1/benchmarks/microbenchmark/pico/pico_benchmark/main.c
@@ -3,26 +3,46 @@
 #include "hardware/clocks.h"
 #include <stdio.h>
 
-#include "benchmark.h"
-
 int main() {
     stdio_init_all();
+    sleep_ms(2000);
 
-    sleep_ms(5000);
-    set_sys_clock_khz(250000, true); // 250 MHz
-    printf("Freq: %lu MHz\n", clock_get_hz(clk_sys) / 1000000);
+    set_sys_clock_khz(80000, true);
 
-    absolute_time_t start, end;
+    printf("Prueba de tiempo\n");
+    printf("----------------\n");
 
-    start = get_absolute_time();
-    test_integers();
-    end = get_absolute_time();
-    printf("Int time: %lld us\n", absolute_time_diff_us(start, end));
+    absolute_time_t inicio, fin;
 
-    start = get_absolute_time();
-    test_floats();
-    end = get_absolute_time();
-    printf("Float time: %lld us\n", absolute_time_diff_us(start, end));
+    // ===== PRUEBA CON ENTEROS =====
+    volatile int sumaEntera = 0;
+
+    inicio = get_absolute_time();
+
+    for (long i = 0; i < 100000000; i++) {
+        sumaEntera = sumaEntera + 1;
+    }
+
+    fin = get_absolute_time();
+
+    int64_t tiempo_int_us = absolute_time_diff_us(inicio, fin);
+
+    printf("Tiempo con enteros: %.3f s\n", tiempo_int_us / 1e6);
+
+    // ===== PRUEBA CON FLOATS =====
+    volatile float sumaFloat = 0;
+
+    inicio = get_absolute_time();
+
+    for (long i = 0; i < 100000000; i++) {
+        sumaFloat = sumaFloat + 1.0f;
+    }
+
+    fin = get_absolute_time();
+
+    int64_t tiempo_float_us = absolute_time_diff_us(inicio, fin);
+
+    printf("Tiempo con floats: %.3f s\n", tiempo_float_us / 1e6);
 
     while (1);
 }
